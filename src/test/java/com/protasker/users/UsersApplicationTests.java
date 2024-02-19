405c661b0271c71779445b304b367bc624b4d1f6
f1iper <ftimofiejew@gmail.com>
2024-02-19 22:38:32 +0100

    Initial user management microservice version

---
+++ src/test/java/com/protasker/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protasker.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
